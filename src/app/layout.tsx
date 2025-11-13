eb4680b454c1a3a562babae4ccebbd15457ec425
tyrantphynix <chirag.rawat1000@gmail.com>
2025-11-13 11:59:12 +0530

    chore: add base template files

---
+++ src/app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+import Footer from '@/components/Footer/Footer'
+import Navbar from '@/components/Navbar/Navbar'
+import ThemeMenu from '@/components/Theme/ThemeMenu'
+import { Fira_Code } from 'next/font/google'
+
+const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
+
+const title = 'John Doe | Full-Stack Web Developer in Chicago'
+
+const description =
+  "Skilled full-stack web developer in Chicago. I build responsive, user-friendly websites with React, NextJS, and NodeJS. Let's bring your vision to life. Hire me today!"
+
+const url = process.env.NEXT_PUBLIC_SITE_URL
+
+export const metadata: Metadata = {
+  title,
+  description,
+  category: 'technology',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+  alternates: {
+    canonical: url,
+  },
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: 'John Doe Portfolio',
+    type: 'website',
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@Basit_Miyanji',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" data-theme="dark">
+      <body className={`${firaCode.className}`}>
+        <header>
+          <Navbar />
+        </header>
+        {children}
+        <ThemeMenu />
+        <Footer />
+      </body>
+    </html>
+  )
+}
